a522d1c302bf8ab3d4a9e862f6fc66b00ec4b469
lostlang <53492781+lostlang@users.noreply.github.com>
2021-11-17 18:34:08 +0300

    Update player

--- src/main/kotlin/ru/sber/orm/repository/PlayerRepository.kt
+++ src/main/kotlin/ru/sber/orm/repository/PlayerRepository.kt
@@ -1,9 +1,32 @@
 package ru.sber.orm.repository
 
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
+import org.hibernate.SessionFactory
 import ru.sber.orm.entity.Player
 
-@Repository
-interface PlayerRepository: CrudRepository<Player, Long> {
+class PlayerRepository(
+    private val sessionFactory: SessionFactory
+) {
+    fun save(player: Player) {
+        sessionFactory.openSession().use { session ->
+            session.beginTransaction()
+            session.persist(player)
+            session.transaction.commit()
+        }
+    }
+
+    fun saveAll(players: MutableList<Player>) {
+        players.forEach{ player ->
+            save(player)
+        }
+    }
+
+    fun findById(id: Long): Player? {
+        val result: Player?
+        sessionFactory.openSession().use { session ->
+            session.beginTransaction()
+            result = session.get(Player::class.java, id)
+            session.transaction.commit()
+        }
+        return result
+    }
 }
